98230b9c7d3926f9f3c3e9ee060aa9b5abc28017
Devin Ercolano <devin.ercolano@gmail.com>
2021-04-22 10:40:31 -0700

    updated website text, minor css changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,38 +1,38 @@
 // portfolio data
 var educationInfo = [{
-        name: "The Evergreen State College",
-        description: "B.S. in Computer Science",
-        graduationDate: "December 2014",
-        location: "Olympia, WA"
-    },
-    
-    {
-    name: "Coding Dojo",
-    description: "Coding Bootcamp",
-    graduationDate: "May 2018",
-    location: "Bellevue, WA"
+    name: "The Evergreen State College",
+    description: "B.S. in Computer Science",
+    graduationDate: "December 2014",
+    location: "Olympia, WA"
+},
+{
+name: "Coding Dojo",
+description: "Coding Bootcamp",
+graduationDate: "May 2018",
+location: "Bellevue, WA"
 }];
 
 var experienceInfo = [{
-    name: "Accord365",
-    company: "Sapient Technologies",
-    location: "Seattle, WA",
-    title: "Intern",
-    dateStarted: "June 2019",
-    dateEnded: "March 2021",
-    description: "I co-facilitated focus group, created questions/study forms and Created a wireframe and consulted with client. I created/updated Postgress database to store user information, created models for database. I implemented both Buffalo and Gin Go frameworks. I developed a ERC-20 token buying site using Solidity, JSON, and JavaScript. I made API calls to Web3 making contract/transaction interactions on a blockchain (also used Truffle-Contracts). I tested the app using Ganache, Metamask, Geth, and Ethereum test networks (Rinkeby and Robsten)"
+name: "Accord365",
+company: "Sapient Technologies",
+location: "Seattle, WA",
+title: "Intern",
+dateStarted: "June 2019",
+dateEnded: "March 2021",
+description: "I co-facilitated the project’s focus group, for the meetings I created questions, study forms, and took notes. I Created a wireframe for the project with Sketch and consulted with client multiple times. I created and updated a Postgress database to store user information, created models for that database. I implemented both Buffalo and Gin frameworks, I used MySQL with Gin. I developed a ERC-20 token buying site using Solidity, JSON, JavaScript, and I also used Truffle-Contracts. I made API calls to web3.js to make transactions on a blockchain. I tested the app using Ganache, Metamask, Geth, and the Ethereum test networks Rinkeby and Robsten."
 }]
 
 var projectsInfo = [{
-    name: "Cyber Defense Game",
-    location: "Olympia, WA",
-    title: "Engineer",
-    dateStarted: "June 2014",
-    dateEnded: "April 2015",
-    description: "I structured and created a SQL database filled with nonce data and wrote scoreboard and submission checker in C. I used Chef to automate tasks on EC2 Linux machine. I Collaborated carefully and clearly to set up a Windows Server 2008 system, with AWS-EC2 for the vulnerability protecting team and invented 60% vulnerabilities for the system attacking team to exploit in a website that connects to the above database."
+name: "Cyber Defense Game",
+location: "Olympia, WA",
+title: "Engineer",
+dateStarted: "June 2014",
+dateEnded: "April 2015",
+description: "I structured and created a SQL database filled with nonce data, wrote a scoreboard, and submission checker in C. I used Chef to automate tasks on an Amazon Web Services EC2 Linux machine. I Collaborated carefully and clearly with a teammate to set up a Windows Server 2008 system (with AWS-EC2) for the vulnerability protecting (Blue) team. I invented 60% of the vulnerabilities for the system attacking (Red) team to exploit in a website that connected to the database mentioned above."
 }]
 
-var skillsData = ['Java', 'MySQL', 'PostgresSQL', 'HTML', 'CSS', 'LATEX', 'Python (Django)', 'JavaScript', 'node.js', 'Solidity', 'Amazon Web Services (EC2/VPC)', 'Vim', 'Git', 'VMware', 'Sketch', 'Windows', 'macOS', 'Android', 'iOS'];
+var skillsData = ['Java', 'MySQL', 'PostgresSQL', 'HTML', 'CSS', 'LATEX', 'Python (Django)', 'JavaScript', 'node.js', 'web3.js API', 'Solidity', 'Amazon Web Services (EC2/VPC)', 'Vim', 'Git', 'VMware', 'Sketch', 'Windows', 'macOS', 'Android', 'iOS'];
+
 
 
 $(window).on('load', function() {
@@ -43,12 +43,12 @@ $(window).on('load', function() {
 
     // Display experienceInfo
     for (i = 0; i < projectsInfo.length; i++) {
-        $('<div class="experience" />').html("<p>" + "<b>" + experienceInfo[i].name + " | " + experienceInfo[i].company + " | " + experienceInfo[i].location + " | " + experienceInfo[i].title + "</b>" + "</p>" + "<br />" + "</p>" + experienceInfo[i].dateStarted + " - " + experienceInfo[i].dateEnded + "</p>" + "<br />" + experienceInfo[i].description).appendTo('#experience-data-container');
+        $('<div class="experience col-lg-2 col-md-4 col-sm-6 col-6" />').html("<p class='info'>" + "<b>" + experienceInfo[i].name + " | " + experienceInfo[i].company + " | " + experienceInfo[i].location + " | " + experienceInfo[i].title + "</b>" + "</p>" + "<br />" + "<p>" + experienceInfo[i].dateStarted + " - " + experienceInfo[i].dateEnded + "</p>" + "<br />" + "<p class='description'>" + experienceInfo[i].description + "</p>").appendTo('#experience-data-container');
     }
 
     // Display projectsInfo
     for (i = 0; i < projectsInfo.length; i++) {
-        $('<div class="project" />').html("<p>" + "<b>" + projectsInfo[i].name + " | " + projectsInfo[i].location + " | " + projectsInfo[i].title + "</b>" + "</p>" +"<br />" + "<p>" + projectsInfo[i].dateStarted + " - " + projectsInfo[i].dateEnded + "</p>" + "<br />" + projectsInfo[i].description).appendTo('#projects-data-container');
+        $('<div class="project" />').html("<p class='info'>" + "<b>" + projectsInfo[i].name + " | " + projectsInfo[i].location + " | " + projectsInfo[i].title + "</b>" + "</p>" +"<br />" + "<p>" + projectsInfo[i].dateStarted + " - " + projectsInfo[i].dateEnded + "</p>" + "<br />" + "<p class='description'>" + projectsInfo[i].description + "</p>").appendTo('#projects-data-container');
     }
 
     // Display skillsData
